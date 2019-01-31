0ae16391fe4ce20d9f04224d2fe0a268b2f309fc
bertw2002 <awan00@stuy.edu>
2019-01-31 12:32:25 -0500

    Fibonacci method

--- recursion.java
+++ recursion.java
@@ -1,4 +1,7 @@
 public class recursion{
+  /*Recursively find the sqrt using Newton's approximation
+  *tolerance is the allowed percent error the squared answer is away from n.
+  *precondition: n is non-negative*/
   public static double sqrt(double x, double tolerance){
     //x / 2 is the guess.
     return SquareRoot(x, x / 2, tolerance);
@@ -16,6 +19,21 @@ public class recursion{
       //recursive part that makes the number closer and closer.
       guess = ( n / guess + guess) / 2;
       return SquareRoot(n, guess, tolerance);
+    }
+  }
+  /*Recursively find the n'th fibbonaci number in linear time
+  *fib(0) = 1; fib(1) = 1; fib(5) = 5
+  *precondition: n is non-negative*/
+  public static int fib(int n){
+    //base cases.
+    if (n == 0){
+      return 0;
+    }if (n == 1){
+      return 1;
+    }
+    //recursive.
+    else{
+      return fib(n - 1) + fib(n - 2);
     }
   }
 
