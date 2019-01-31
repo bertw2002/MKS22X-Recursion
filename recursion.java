0b1d5313bace9995a6360b63387bf2b0f9690ba4
bertw2002 <awan00@stuy.edu>
2019-01-31 12:18:09 -0500

    Added tolerance parameter.g

--- recursion.java
+++ recursion.java
@@ -1,21 +1,22 @@
 public class recursion{
-  public static double sqrt(double x){
+  public static double sqrt(double x, double tolerance){
     //x / 2 is the guess.
-    return SquareRoot(x, x / 2);
+    return SquareRoot(x, x / 2, tolerance);
   }
   //private helper for sqrt.
   private static double percentError(double n, double guess){
     //percent error formula.
     return ((guess * guess - n) / n) * 100;
   }
-  private static double SquareRoot(double n, double guess){
-    if (Math.abs(percentError(n, guess)) <= 0.001){
+  private static double SquareRoot(double n, double guess, double tolerance){
+    if (Math.abs(percentError(n, guess)) <= tolerance){
       return guess;
     }
     else{
       //recursive part that makes the number closer and closer.
       guess = ( n / guess + guess) / 2;
-      return SquareRoot(n, guess);
+      return SquareRoot(n, guess, tolerance);
     }
   }
+
 }
