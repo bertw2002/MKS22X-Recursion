fd0537ddd68189033209fbfe0d3ec8ea0c8309ce
bertw2002 <awan00@stuy.edu>
2019-01-31 12:04:44 -0500

    sqrt method finished

--- recursion.java
+++ recursion.java
@@ -1,1 +1,14 @@
-
+public class recursion{
+  public static double sqrt(double x){
+    return SquareRoot(x, x - 1);
+  }
+  public static double SquareRoot(double n, double guess){
+    if (Math.abs(guess * guess - n) <= 0.00001){
+      return guess;
+    }
+    else{
+      guess = ( n / guess + guess) / 2;
+      return SquareRoot(n, guess);
+    }
+  }
+}
