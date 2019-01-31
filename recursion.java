5de36002bb89ca0b0c9c27464a1e03160d2d46ed
bertw2002 <awan00@stuy.edu>
2019-01-31 12:14:38 -0500

    Changed the base case to percent error. Also added comments.

--- recursion.java
+++ recursion.java
@@ -1,12 +1,19 @@
 public class recursion{
   public static double sqrt(double x){
-    return SquareRoot(x, x - 1);
+    //x / 2 is the guess.
+    return SquareRoot(x, x / 2);
   }
-  public static double SquareRoot(double n, double guess){
-    if (Math.abs(guess * guess - n) <= 0.00001){
+  //private helper for sqrt.
+  private static double percentError(double n, double guess){
+    //percent error formula.
+    return ((guess * guess - n) / n) * 100;
+  }
+  private static double SquareRoot(double n, double guess){
+    if (Math.abs(percentError(n, guess)) <= 0.001){
       return guess;
     }
     else{
+      //recursive part that makes the number closer and closer.
       guess = ( n / guess + guess) / 2;
       return SquareRoot(n, guess);
     }
