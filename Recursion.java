e0bba3d4a1438a1d1c2cc64cf00db6fd0b8c7c48
bertw2002 <awan00@stuy.edu>
2019-02-03 13:11:49 -0500

    Finished makeallsums code. Don't know if it works yet.

--- Recursion.java
+++ Recursion.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class recursion{
+public class Recursion{
   private ArrayList<Integer> arraySums;
   public ArrayList<Integer> getarraySums(){
     return arraySums;
@@ -9,21 +9,21 @@ public class recursion{
   *precondition: n is non-negative*/
   public static double sqrt(double x, double tolerance){
     //x / 2 is the guess.
-    return SquareRoot(x, x / 2, tolerance);
+    return sqrtHelper(x, x / 2, tolerance);
   }
   //private helper for sqrt.
   public static double percentError(double n, double guess){
     //percent error formula.
     return ((guess * guess - n) / n) * 100;
   }
-  public static double SquareRoot(double n, double guess, double tolerance){
+  public static double sqrtHelper(double n, double guess, double tolerance){
     if (percentError(n, guess) <= tolerance && percentError(n, guess) >= -1 * tolerance){
       return guess;
     }
     else{
       //recursive part that makes the number closer and closer.
       guess = ( n / guess + guess) / 2;
-      return SquareRoot(n, guess, tolerance);
+      return sqrtHelper(n, guess, tolerance);
     }
   }
   /*Recursively find the n'th fibbonaci number in linear time
@@ -43,10 +43,30 @@ public class recursion{
   }
   /*As Per classwork*/
   public static ArrayList<Integer> makeAllSums(int n){
-    return allSumsHelper(n, 0);
+    ArrayList<Integer> arrList = new ArrayList<Integer>();
+    arrList.add(0);
+    return allSumsHelper(n, arrList);
   }
-  public static ArrayList<Integer> allSumsHelper(int total, int sum){
 
+  public static ArrayList<Integer> allSumsHelper(int total, ArrayList<Integer> arrList){
+    //base case.
+    if (total == 0) return arrList;
+    else{
+      allSumsHelper(total - 1, arrList);
+      int lenlist = arrList.size();
+      for (int x = 0; x < lenlist; x++){
+        if (!(arrList.contains(arrList.get(x) + total))){
+          arrList.add(arrList.get(x) + total);
+        }
+      }
+    }
+    return arrList;
   }
+  public static void main(String[] args) {
+     System.out.println(makeAllSums(0));
+     System.out.println(makeAllSums(6));
+     System.out.println(makeAllSums(3));
+
+   }
 
 }
